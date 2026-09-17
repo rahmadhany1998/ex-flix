65c2a5abcfb3cc4c490abf8b2066e61fb72501f2
Javiera Rahmadhany <hellorahmadhany@gmail.com>
2026-09-17 20:44:14 +0800

    feat: create plan page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\SubscribeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/subscribe/plans', [SubscribeController::class, 'showPlans'])->name('subscribe.plans');
