c24141703ff1ed1e22434bcb6b4045eb22214093
Javiera Rahmadhany <hellorahmadhany@gmail.com>
2026-09-17 20:12:11 +0800

    feat: make migration and seeder for plan and membership

---
+++ app/Models/Membership.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Membership extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'plan_id',
+        'start_date',
+        'end_date',
+        'active',
+    ];
+
+    public function plan(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Plan::class);
+    }
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
