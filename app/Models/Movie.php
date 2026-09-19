7ea0c99b8e12e835284885e612cb65bb677c35cf
Javiera Rahmadhany <hellorahmadhany@gmail.com>
2026-09-19 19:01:31 +0800

    feat: create model, migration and seeder for rating

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -29,4 +29,14 @@ class Movie extends Model
     {
         return $this->belongsToMany(Category::class);
     }
+
+    public function ratings(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Rating::class);
+    }
+
+    public function getAverageRatingAttribute(): float
+    {
+        return $this->ratings()->avg('rating');
+    }
 }
