9197f4beb1d430a061f9e95ca77353f4435fa25b
Javiera Rahmadhany <hellorahmadhany@gmail.com>
2026-09-21 19:43:33 +0800

    feat: event and listener : send notification for expired member via email

---
+++ app/Events/MembershipHasExpired.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Membership;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MembershipHasExpired
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public Membership $membership)
+    {
+        //
+    }
+
+    
+}
---
+++ app/Notifications/MembershipExpiredNotification.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class MembershipExpiredNotification extends Notification
+{
+    use Queueable;
+
+    private $membership;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($membership)
+    {
+        $this->membership = $membership;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Membership Expired | Codeflix')
+            ->greeting('Hello!')
+            ->line('Your membership has expired.')
+            ->line('Expired Date: ' . $this->membership->end_date->format('d M Y'))
+            ->action('Renew Membership', url('/renew'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
